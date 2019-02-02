f26293ce594ca7e8d31b012ca405eb53ef28aee9
jilen <jilen.zhang@gmail.com>
2019-02-02 10:39:26 +0800

    fix release script

--- build/release.sh
+++ build/release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sbt -Dplay.version="2.6.21" release cross with-defaults
-sbt -Dplay.version="2.7.0" release cross with-defaults
+sbt -Dplay.version="2.6.21" "release cross with-defaults"
+sbt -Dplay.version="2.7.0" "release cross with-defaults"
